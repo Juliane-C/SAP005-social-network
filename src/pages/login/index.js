2e1312185e1148d20b7dedcba9ced79abf16ab67
Juliane-C <juliane.cristinara97@gmail.com>
2021-01-05 14:39:09 -0300

    testando código

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,7 +2,16 @@ export const Login = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
-      <h1>Login!</h1>
+  <input id="user-email" type="email" placeholder="Insira seu e-mail" required> E-mail:</input>
+  <input id="user-password" type="password" placeholder="Insira sua senha" required> Senha:</input>
+  <button id="login-btn">Fazer login</button>
   `;
+  const loginBtn = rootElement.querySelector("#login-btn")
+  loginBtn.addEventListener("click", () => {
+  let userEmail = rootElement.querySelector("#user-email").value
+  let userPassword = rootElement.querySelector("#user-password").value
+  console.log(userEmail, userPassword)
+  })
+  
   return rootElement;
 };
