3cf63fcbf5a0f24fe71e6b48fa13149a93d9049c
letbrasil <le.brasileiro@gmail.com>
2021-01-04 22:30:58 -0300

    cria usuários e faz login com firebase

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,19 +1,45 @@
+import { googleLogin } from '../../services/index.js';
+
 export const Register = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
       <h1>Cadastro!</h1>
-      <input id="username" type="text" placeholder="Seu nome ou apelido" required>
-      <input id="new-email" type="email" placeholder="Seu e-mail" required>
-      <input id="new-password" type="password" placeholder="Senha" required>
-      <button id="register-btn">Cadastrar</button>
-  `;
-  const registerBtn = rootElement.querySelector("#register-btn")
-  registerBtn.addEventListener("click", () => {
-    const username = rootElement.querySelector("#username").value
-    const email = rootElement.querySelector("#new-email").value
-    const password = rootElement.querySelector("#new-password").value
-    console.log(username, email, password)
-  })
+      <form>
+        <input id="username" type="text" placeholder="Seu nome ou apelido" required><br>
+        <input id="new-email" type="email" placeholder="Seu e-mail" required><br>
+        <input id="new-password" type="password" placeholder="Senha" required><br>
+        <button id="register-btn">Cadastrar</button>
+        <p>-- ou --</p>
+        <button id="google-signup">Continuar com Google</button>
+        <p>Já possui conta? <a href="/login">Entrar</a></p>
+      </form>
+  `;          // SPA - Corrigir href acima para evitar recarregamento da página!
+
+  const registerBtn = rootElement.querySelector('#register-btn');
+  registerBtn.addEventListener('click', (e) => {      // Mover função de cadastro para services!
+    e.preventDefault();
+    const username = rootElement.querySelector('#username').value;
+    const email = rootElement.querySelector('#new-email').value;
+    const password = rootElement.querySelector('#new-password').value;
+    console.log(username, email, password);
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        // Signed in
+        // ...
+        console.log('Deu bom! :D', user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        console.log('Deu ruim! :(', errorCode, errorMessage);
+      });
+  });
+
+  const googleSignUp = rootElement.querySelector('#google-signup');
+  googleSignUp.addEventListener('click', googleLogin);
+  
   return rootElement;
 };
--- src/services/index.js
+++ src/services/index.js
@@ -4,3 +4,31 @@ export const myFunction = () => {
   // seu código aqui
   console.log('Olá mundo!');
 };
+
+export const googleLogin = (e) => {
+  e.preventDefault();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const token = result.credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+      console.log('Googlou!') 
+      console.log(token, user);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      const credential = error.credential;
+      // ...
+      console.log('Googlou não...');
+      console.log(errorCode, errorMessage, email, credential);
+    });
+};
